259a4b1bd0cbe4771394a96e6cc0b08842ce2c28
Abdullah Mahdi <134118222+Abdullah-Mahdi272@users.noreply.github.com>
2024-06-08 14:09:42 -0400

    Moved contents to Main.tsx

--- personalPort/src/App.tsx
+++ personalPort/src/App.tsx
@@ -1,67 +1,25 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
 import Carousel from "./components/Carousel";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import First from "./components/Redirects/First";
+import Second from "./components/Redirects/Second";
+import Main from "./components/Redirects/Main";
 // import Test from "./components/Test";
 
 const App: React.FC = () => {
   return (
     <>
       <Sidebar />
-      {<p>Hi</p>}
+      {/* <p>Hi</p> */}
 
-      <div className="content">{}</div>
-
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-
-      <Carousel />
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
-      <p>Hi</p>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Main />} />
+          <Route path="/" element={<Main />} />
+          <Route path="/link" element={<Second />} />
+        </Routes>
+      </Router>
     </>
   );
 };
